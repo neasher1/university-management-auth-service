8a1058d05742267a7ebde9b382fdf076f906ca92
neasher <neasher75@gmail.com>
2023-05-31 21:53:58 +0600

    setup eslint, prettier, huskey, lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express, { Application, Request, Response } from 'express';
-const app: Application = express();
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+import cors from 'cors'
 
-app.use(cors());
+app.use(cors())
 
 //parser
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Server is running')
-});
+  res.send('Server is running')
+})
 
-export default app;
+export default app
