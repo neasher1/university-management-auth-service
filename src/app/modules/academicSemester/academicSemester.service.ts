16d06a52d6b54a3c833d071ca9eaa8feacf42988
neasher <neasher75@gmail.com>
2023-06-24 17:58:22 +0600

    [DONE] pagination setup and dynamic sorting

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -3,6 +3,10 @@ import ApiError from '../../../error/ApiError';
 import { academicSemesterTitleCodeMapper } from './academicSemester.constants';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
+import { IPaginationOptions } from '../../../interfaces/paginationOptions';
+import { IGenericResponse } from '../../../interfaces/common';
+import { paginationHelpers } from '../../../helpers/paginationHelpers';
+import { SortOrder } from 'mongoose';
 
 const createSemester = async (
   payload: IAcademicSemester
@@ -16,6 +20,39 @@ const createSemester = async (
   return result;
 };
 
+const getAllSemesters = async (
+  paginationOptions: IPaginationOptions
+): Promise<IGenericResponse<IAcademicSemester[]>> => {
+  // const { page = 1, limit = 10 } = paginationOptions;
+  // const skip = (page - 1) * limit;
+
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(paginationOptions);
+
+  const sortConditions: { [key: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
+
+  const result = await AcademicSemester.find()
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit);
+
+  const total = await AcademicSemester.countDocuments();
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 export const AcademicSemesterService = {
   createSemester,
+  getAllSemesters,
 };
